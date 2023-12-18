b814bf1c2adc49b36ab1aee0e053534ca495d7ac
OumaArera <johnouma999@gmail.com>
2023-12-18 11:32:47 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+
+
+const superbowlWin = arr => {
+    let winningYear = arr.find(obj => obj.result === "W");
+
+    if (winningYear){
+        return winningYear.year;
+    }else{
+        return undefined;
+    }
+}
+
